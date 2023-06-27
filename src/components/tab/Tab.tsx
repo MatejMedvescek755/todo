3c37d26a963c0aef1b6702f6e60f05990b3a0db0
Matej <matejmedvescek1@gmail.com>
2023-06-27 13:14:04 +0200

    added design and a component for tab

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,9 +1,14 @@
 import { TabProps } from "."
+import  Checkmark  from "../../assets/Checkmark.tsx"
+
 
 const Tab = ({ todo, completed, userId }: TabProps)=>{
     return(
-        <div className="">
+        <div className="bg-white text-black mb-4 p-4 rounded-md flex w-[50vw] justify-between">
             <p>{todo}</p>
+            <div className="flex ">
+                {completed && <Checkmark />}
+            </div>
         </div>
     )
 }
