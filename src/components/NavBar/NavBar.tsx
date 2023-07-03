78c2b0d4b5bfbd8b401b4e8b771a8c7841adbea7
Matej <matejmedvescek1@gmail.com>
2023-07-03 14:42:39 +0200

    fixed overlapping NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
 
     return (
-        <div className="w-screen h-[7vh] sticky bg-emerald-500 flex font-body tracking-[1px] leading-[1.27em] text-sm">
+        <div className="max-w-[100vw] h-[7vh] sticky bg-emerald-500 flex font-body tracking-[1px] leading-[1.27em] text-sm">
             <Link to={"/"}>
                 <div className="p-2 w-[4vw] h-full flex items-center justify-center hover:bg-emerald-600 cursor-pointer active:text-xs hover:underline underline-offset-2" ><p>HOME</p></div>
             </Link>
