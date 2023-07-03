a8757a4c93b29d1847788e8710b2fa3dd037db69
Matej <matejmedvescek1@gmail.com>
2023-07-03 10:35:46 +0200

    added DeleteTodoComponent

---
+++ src/components/DeleteTodoComponent/DeleteTodoComponent.tsx
@@ -0,0 +1,25 @@
+import { DeleteTodoProps } from "."
+
+
+
+const DeleteTodoComponent = ({ setState }: DeleteTodoProps) => {
+    function handleClick(event: React.MouseEvent) {
+        console.log("delete")
+        setState(false)
+    }
+    return (
+        <div>
+            <div className="w-[20vw] h-[30vh] p-4 bg-white text-black flex  flex-col justify-between items-start rounded-lg">
+                <h1 className="p-2 font-black text-xl">
+                    Are you sure you want to delete your Todo
+                </h1>
+                <div className="w-[18vw] h-[5vh] items-center flex justify-between">
+                    <button className="h-[5vh] w-[4vw] border-2 border-black rounded-lg p-2 hover:text-white hover:bg-black active:text-sm" onClick={handleClick}>delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default DeleteTodoComponent
---
+++ src/components/DeleteTodoComponent/index.ts
@@ -0,0 +1,8 @@
+import DeleteTodoComponent from "./DeleteTodoComponent"
+
+export interface DeleteTodoProps{
+    setState:React.Dispatch<boolean>
+}
+
+
+export default DeleteTodoComponent
