c63bdf4770cb5ac46fc5c14d8b6a72821a5e7670
Matej <matejmedvescek1@gmail.com>
2023-07-03 14:44:55 +0200

    added api support for getting users

--- src/index.ts
+++ src/index.ts
@@ -26,10 +26,17 @@ export const addItem = async (todo: string, completed?: boolean,
     return jsonData
 }
 
-export const deleteItem =async (id:number) => {
+export const deleteItem = async (id:number) => {
     const response = await fetch(`https://dummyjson.com/todos/${id}`, {method: 'DELETE',})
     const jsonData = await response.json()
     return jsonData
+}
+
+export const getUsers = async () => {
+    const response = await fetch('https://dummyjson.com/users')
+    const jsonData = await response.json()
+    console.log(jsonData.users)
+    return jsonData.users
 }
 
 export interface Todos{
@@ -46,3 +53,69 @@ export interface Todo{
     userId: number,
     isDeleted?:boolean
 }
+
+export interface User {
+    id: number
+    firstName: string
+    lastName: string
+    maidenName: string
+    age: number
+    gender: string
+    email: string
+    phone: string
+    username: string
+    password: string
+    birthDate: string
+    image: string
+    bloodGroup: string
+    height: number
+    weight: number
+    eyeColor: string
+    hair: Hair
+    domain: string
+    ip: string
+    address: Address
+    macAddress: string
+    university: string
+    bank: Bank
+    company: Company
+    ein: string
+    ssn: string
+    userAgent: string
+  }
+  
+  export interface Hair {
+    color: string
+    type: string
+  }
+  
+  export interface Address {
+    address: string
+    city: string
+    coordinates: Coordinates
+    postalCode: string
+    state: string
+  }
+  
+  export interface Coordinates {
+    lat: number
+    lng: number
+  }
+  
+  export interface Bank {
+    cardExpire: string
+    cardNumber: string
+    cardType: string
+    currency: string
+    iban: string
+  }
+  
+  export interface Company {
+    address: Address
+    department: string
+    name: string
+    title: string
+  }
+  
+
+  
