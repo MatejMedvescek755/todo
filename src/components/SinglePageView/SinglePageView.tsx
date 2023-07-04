7a42dbc7aeb8c108ad84fd26bd9c90272953b9fc
Matej <matejmedvescek1@gmail.com>
2023-07-04 10:08:40 +0200

    displayed todo data

--- src/components/SinglePageView/SinglePageView.tsx
+++ src/components/SinglePageView/SinglePageView.tsx
@@ -3,31 +3,73 @@ import EditTodoComponent from "../EditTodoComponent/EditTodoComponent"
 import Modal from "../Modal/Modal"
 import React from "react"
 import NavBar from "../NavBar/NavBar"
+import { useParams } from "react-router-dom"
+import { Todo, getItem } from "../.."
+import Checkmark from "../../assets/Checkmark"
 
-const SingleTodoView = () =>{
-    const [isOpenEdit, setIsOpenEdit ] = React.useState<boolean>(false)
-    const [ isOpenDelete, setIsOpenDelete ] = React.useState<boolean>(false)
- 
+const SingleTodoView = () => {
+    const [isOpenEdit, setIsOpenEdit] = React.useState<boolean>(false)
+    const [isOpenDelete, setIsOpenDelete] = React.useState<boolean>(false)
+    const { id } = useParams()
+    const [todo, setTodo] = React.useState<Todo>()
+    const [error, setError] = React.useState<any>();
+    const [text, setText] = React.useState<string>()
 
-    return(
-        // <Modal >
-        //     <EditTodoComponent />
-        // </Modal>
-        <div className="w-screen h-screen">
-            <NavBar/>
-            <div className="flex w-[20vw] justify-around">
-            <button className='mt-6 min-w-fit border-2 p-2 border-white rounded-md' onClick={()=>setIsOpenEdit(true)}>edit</button>
-            <button className='mt-6 min-w-fit border-2 p-2 border-white rounded-md'onClick={()=>setIsOpenDelete(true)}>delete</button>
+    React.useEffect(() => {
+        try {
+            loader(parseInt(id)).then((res) => {
+                setTodo(res);
+            })
+        } catch (error) {
+            console.error(error)
+            setError(error)
+        }
+
+        return () => {
+            setError(undefined)
+        }
+    }, [id])
+
+    if (error)
+        return <div>error {error}</div>
+    if(todo === undefined)
+        return <div>loading ...</div>
+    return (
+        <div>
+            <NavBar />
+            <div className="w-full h-[95vh] flex justify-center items-center">
+                <div className="shadow-xl shadow-gray-700 p-4 pb-8 flex flex-col justify-between
+                 h-[45vh] rounded-lg bg-white">
+                    <div className="w-[25vw] justify-center text-black p-2 flex">
+                        <div><p className="font-mono text-lg">{todo.todo}</p></div>
+                        <div className="ml-4">{todo.completed ? <Checkmark  /> : ""}</div>
+                    </div>
+                    <div className="flex w-[25vw] items-center flex-col p-2 ">
+                        <button className='mt-6 h-[5vh] w-[15vw] text-black min-w-fit border-2 p-2 border-black rounded-md' onClick={() => setIsOpenEdit(true)}>edit
+                        </button>
+                        <button className='mt-6 h-[5vh] w-[15vw] text-black min-w-fit border-2 p-2 border-black rounded-md' onClick={() => setIsOpenDelete(true)}>delete
+                        </button>
+                    </div>
+                </div>
+
+                <Modal isOpen={isOpenEdit}>
+                    <EditTodoComponent todo={todo} setState={setIsOpenEdit} />
+                </Modal>
+                <Modal isOpen={isOpenDelete}>
+                    <DeleteTodoComponent setState={setIsOpenDelete} />
+                </Modal>
             </div>
-
-            <Modal isOpen={isOpenEdit}>
-                <EditTodoComponent setState={setIsOpenEdit} />
-            </Modal>
-            <Modal isOpen={isOpenDelete}>
-                <DeleteTodoComponent setState={setIsOpenDelete}/>
-            </Modal>
         </div>
     )
+}
+
+const loader = async (id: number) => {
+    try {
+        const todo: Todo = await getItem(id)
+        return todo
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 
