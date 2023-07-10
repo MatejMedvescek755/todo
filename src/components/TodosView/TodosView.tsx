20419cf68a451c1ccf5132abcd57734b349fece3
Matej <matejmedvescek1@gmail.com>
2023-07-10 18:00:02 +0200

    changed add todo form

--- src/components/TodosView/TodosView.tsx
+++ src/components/TodosView/TodosView.tsx
@@ -7,6 +7,7 @@ import React from 'react';
 import { deleteItem } from "../../index.ts"
 import Modal from '../Modal/index.ts';
 import EmptyInputModal from '../EmptyInputModal/EmptyInputModal.tsx';
+import Plus from '../../assets/Plus.tsx';
 
 
 function App() {
@@ -14,13 +15,15 @@ function App() {
   const [text, setText] = React.useState<string>("")
   const [error, setError] = React.useState<any>();
   const [confirmModal, setConfirmModal] = React.useState<boolean>(false)
+  const [showAdd, setShowAdd] = React.useState<boolean>(false)
 
-
-  async function handleClick() {
-    if(text  !== ""){
+  async function handleKey(event) {
+    console.log(event.key)
+    if(event.key !== "Enter") return
+    if ( text !== "") {
       try {
         const req = await addItem(text)
-        if(!todos) return
+        if (!todos) return
         const newObject = {
           todos: [req].concat(todos["todos"]),
           total: todos["total"],
@@ -31,22 +34,22 @@ function App() {
         setText("")
       } catch (error) {
         console.error(error)
-        setError(error) 
+        setError(error)
       }
-    }else{
+    } else {
       setConfirmModal(true)
     }
   }
 
-  const onDeleteHandler = async (id:number) => {
+  const onDeleteHandler = async (id: number) => {
     try {
       const obj = await deleteItem(id)
-      if(!todos) return
-      const list = todos["todos"].map((el)=>{
-        if(el.id == id){
-            return obj
-        }else{
-            return el
+      if (!todos) return
+      const list = todos["todos"].map((el) => {
+        if (el.id == id) {
+          return obj
+        } else {
+          return el
         }
       })
       const newTodo = {
@@ -55,7 +58,7 @@ function App() {
         skip: todos["skip"],
         limit: todos["limit"]
       }
-    setTodos(newTodo) 
+      setTodos(newTodo)
     } catch (error) {
       console.error(error)
       setError(error)
@@ -83,29 +86,30 @@ function App() {
     <>
       <div id="main" className='flex flex-col items-center flex-wrap max-w-[100vw] min-h-[95vh]'>
         <Modal isOpen={confirmModal}>
-          <EmptyInputModal setState={setConfirmModal}></EmptyInputModal>  
+          <EmptyInputModal setState={setConfirmModal}></EmptyInputModal>
         </Modal>
-        <div className='flex w-[55vw] h-[10vh] flex-row justify-center items-end'>
-          <div className='flex flex-col mb-4 w-[50vw]'>
-            <label className='capitalize' htmlFor="add">add tasks</label>
-            <input className='2xl:w-[50vw] xl:w-[49vw] lg:w-[48vw]
-            sm:w-[45vw] w-[42vw]  rounded-sm p-2' type="text" name="add" id="add" value={text} onChange={(e) => setText(e.target.value)} autoComplete='off'/>
+        <div className='flex w-[55vw] h-[10vh]'>
+          <div className="fixed bottom-40 right-60 bg-emerald-500 rounded-full w-[50px] h-[50px] flex items-center justify-center hover:cursor-pointer hover:bg-emerald-400"
+            onClick={() => { setShowAdd(true) }}>
+            <Plus />
           </div>
-          <div className='flex justify-center items-center w-[5vw] h-[10vh]'>
-            <button className='mt-6 min-w-fit border-2 p-2 border-white rounded-md' onClick={handleClick} >confirm</button>
+          {showAdd && <div className='flex flex-col w-[50vw]'>
+            <input className='2xl:w-[55vw] xl:w-[49vw] h-[10vh] lg:w-[48vw]
+            sm:w-[45vw] w-[42vw] bg-[#242424] outline-none text-lg  rounded-sm p-2' autoFocus type="text" name="add" id="add" placeholder='text for your todo, Press enter to submit' value={text} onChange={(e) => setText(e.target.value)} autoComplete='off' onKeyDown={handleKey} />
           </div>
+          }
         </div>
         <div className='flex flex-wrap justify-center items-start justify-center content-center w-[55vw] flex-col'>
           {todos ? <div>{
-            todos["todos"].map((todo:Todo) => {
-            if (!todo.isDeleted) {
-              return(
+            todos["todos"].map((todo: Todo) => {
+              if (!todo.isDeleted) {
+                return (
 
-                <SingleTodoView key={todo.id} todo={todo} onDeleteHandler={onDeleteHandler} />
+                  <SingleTodoView key={todo.id} todo={todo} onDeleteHandler={onDeleteHandler} />
 
-              )
-            }
-          })
+                )
+              }
+            })
           }</div> : <p>loading</p>}
         </div>
       </div>
